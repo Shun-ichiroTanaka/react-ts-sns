c2fcf2b4f8e4cde2877972606c5db85ab7c08673
田中 俊一朗 <shun.ichiro.tanaka16@gmail.com>
2021-01-25 00:09:01 +0900

    GraphqlのQuery設定

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # V.1.0.0
 
-・Apollo,Expressでサーバーを立ち上げ
+### ApolloServer,Expressで開発サーバー立ち上げ 2021/1/25
+
+localhost:4000 を開発環境に設定
+
+### 開発環境でGraphqlを使えるようにする
+
+localhost:4000/graphql でGraphqlのQueryを確認できるように
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import { buildSchema } from "type-graphql";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send("どうも");
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ HelloResolver] ,
+      validate: false
+    })
   });
+
+  apolloServer.applyMiddleware({ app });
   
   app.listen(4000, () => {
     console.log('localhost:4000でサーバーを立ち上げ')
